b60d4a8fa13ada4fdf43c5110060090f53d17280
Molnar, Tamas <tamas.molnar@juliusbaer.com>
2023-08-02 19:49:42 +0200

    initial commit

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Hero } from '../Hero';
+import { HEROES } from '../mock-heroes';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+
+export class HeroesComponent {
+  heroes = HEROES;
+}
