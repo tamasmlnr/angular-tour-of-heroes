7280a19f80280079ad5c00b2c9b350b9de8df675
Molnar, Tamas <tamas.molnar@juliusbaer.com>
2023-08-06 20:09:47 +0200

    hero search + use fake API

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,35 +1,42 @@
-import { Component, Input } from '@angular/core';
-import { Hero } from '../Hero';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 
+import { Hero } from '../Hero';
 import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
-  styleUrls: ['./hero-detail.component.css']
+  styleUrls: [ './hero-detail.component.css' ]
 })
-export class HeroDetailComponent {
+export class HeroDetailComponent implements OnInit {
+  hero: Hero | undefined;
+
   constructor(
     private route: ActivatedRoute,
     private heroService: HeroService,
     private location: Location
   ) {}
-  @Input() hero?: Hero;
 
   ngOnInit(): void {
     this.getHero();
   }
 
-  goBack(): void {
-    this.location.back();
-  }
-  
   getHero(): void {
-    const id = Number(this.route.snapshot.paramMap.get('id'));
+    const id = parseInt(this.route.snapshot.paramMap.get('id')!, 10);
     this.heroService.getHero(id)
       .subscribe(hero => this.hero = hero);
   }
 
+  goBack(): void {
+    this.location.back();
+  }
+
+  save(): void {
+    if (this.hero) {
+      this.heroService.updateHero(this.hero)
+        .subscribe(() => this.goBack());
+    }
+  }
 }
